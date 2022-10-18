55e4a9707e35e2638d21b5d3484ff34d9e267ec4
leejeanah <ijina590@naver.com>
2022-10-18 16:29:45 +0900

    02
    temp = 1
    temp = 1
    temp = 1
    temp = 1
    temp = 1

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 
 int main(void)
 {
-	int flag = 1;
-	int y; 
+	int i;
 	
-	while(flag != 0)
+	for(i=0; i<5; i++)
 	{
-		y = 3;
-		flag = 0;
+		int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
 	}
-	y = 4;
 	return 0;
 }
