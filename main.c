25528fbce823acb0209678960a366239cc36efec
leejeanah <ijina590@naver.com>
2022-10-18 17:11:12 +0900

    06-1

--- main.c
+++ main.c
@@ -3,25 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void sub(void);
- 
- int main(void)
- {
- 	int i;
- 	for(i=0; i<3; i++)
- 	 sub();
- 	return 0;
- }
- void sub(void)
- {
- 	int auto_count = 0; //자동할당 
- 	static int static_count = 0; // 정적할당
-	
-	//변수 1 증가 
- 	auto_count++;
- 	static_count--;
- 	
- 	//변수 내용 확 
- 	printf("auto_count=%d\n", auto_count);
- 	printf("static_count=%d\n", static_count);
- }
+int all_files;
+
+extern void sub();
+
+int main(void)
+{
+	sub();
+	printf("%d\n", all_files);
+	return 0;
+ } 
