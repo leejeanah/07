235e5ea9f532fc77fcb458a65e49563452c8a5ee
leejeanah <ijina590@naver.com>
2022-10-18 16:39:15 +0900

    03

--- main.c
+++ main.c
@@ -3,15 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int inc(int counter);  // 함수 정 
+
 int main(void)
 {
-	int i;
+	int i = 10;
 	
-	for(i=0; i<5; i++)
-	{
-		int temp = 1;
-		printf("temp = %d\n", temp);
-		temp++;
-	}
+	printf("함수 호출전 i=%d\n", i);
+	inc(i);
+	printf("함수 호출후 i=%d\n", i);
 	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
